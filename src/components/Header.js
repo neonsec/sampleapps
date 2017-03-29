d2a926053cb7d4a830883e5f470b86176637f39f
fahidroid <fahidroid@gmail.com>
2017-03-29 12:26:08 +0530

    abcdef

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles;
 
   return (
     <View style={viewStyle}>
-    <Text style={textStyle}>Albums</Text>
+    <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
@@ -14,13 +14,19 @@ const styles = {
   textStyle: {
     fontSize: 20,
     textAlign: 'center',
-    color: '#fff',
-    fontStyle: 'italic',
+      fontStyle: 'italic',
     marginTop: 10,
     marginBottom: 10
   },
   viewStyle: {
-    backgroundColor: '#f00'
+    backgroundColor: '#f3f3f3',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 2,
+    position: 'relative'
   }
 };
 export default Header;
